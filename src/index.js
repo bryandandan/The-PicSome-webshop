51e9f5f13bcd18007bc67677b938034928a162b9
Bryan <bryanfouad.dandan@gmail.com>
2020-04-23 17:44:34 +0300

    added the css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
+
+import {ContextProvider} from "./Context"
+import App from "./App"
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>, 
+  <ContextProvider>
+      <Router>
+          <App />
+      </Router>
+  </ContextProvider>, 
   document.getElementById("root")
 )
 
